8ef095f9eb73fbfac640801ebe5ac7375b4a55a5
Ara <aro077a@gmail.com>
2021-11-24 00:15:03 +0400

    added testing with Jest and Enzyme

---
+++ src/redux/auth/authReducer.ts
@@ -0,0 +1,29 @@
+import {
+  AuthActionTypes,
+  LOGIN_USER_SUCCESS,
+} from './actionTypes'
+import { AccessTokenType } from './model'
+
+export interface State {
+  isLoggedIn: boolean
+  sessionToken: AccessTokenType | null
+}
+
+export const initialState: State = {
+  isLoggedIn: false,
+  sessionToken: null,
+}
+
+export const authReducer = (state: State = initialState, action: AuthActionTypes) => {
+  switch (action.type) {
+  case LOGIN_USER_SUCCESS:
+    return {
+      ...state,
+      sessionToken: action.payload,
+      isLoggedIn: true,
+    }
+
+  default:
+    return state
+  }
+}
